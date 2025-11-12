a2824a66ae1cc44a10dd2623e76c7ccef9b93a9b
ethanbrook-dev <ethanbrook314@gmail.com>
2025-11-11 22:37:19 -0500

    rror in C:\Users\ethan\Columns-MIPS\Columns.asm line 115: Runtime exception at 0x00400100: address out of range 0x50029080

--- Columns.asm
+++ Columns.asm
@@ -24,12 +24,10 @@ column_colors:   .word 0, 0, 0  # Top, middle, bottom gem
 .globl main
 main:
     # Initialize first column
-    jal generate_random_colors
 
 game_loop:
     jal clear_screen
     jal draw_border
-    jal draw_static_gems
     jal draw_current_column
 
     # Sleep ~16ms for ~60 FPS
@@ -38,25 +36,6 @@ game_loop:
     syscall
 
     j game_loop
-
-# -------------------------------
-# Generate 3 random colors for column
-# -------------------------------
-generate_random_colors:
-    la $t0, column_colors
-    li $t1, 3
-
-gen_loop:
-    li $v0, 42
-    li $a0, 0
-    li $a1, 6
-    syscall
-    addi $v0, $v0, 1
-    sw $v0, 0($t0)
-    addi $t0, $t0, 4
-    addi $t1, $t1, -1
-    bgtz $t1, gen_loop
-    jr $ra
 
 # -------------------------------
 # Clear screen
@@ -77,17 +56,20 @@ clear_loop:
 # Draw border (unit-based)
 # -------------------------------
 draw_border:
-    li $a2, 7          # Gray
+    addi $sp, $sp, -4          # make space on stack
+    sw $ra, 0($sp)             # save return address
 
-    # Top and bottom border
+    li $a2, 7                  # Gray
+
+    # --- Top border ---
     li $a1, 0
-top_border:
     li $a0, 0
 top_border_loop:
     jal draw_pixel
     addi $a0, $a0, 1
     ble $a0, 11, top_border_loop
 
+    # --- Bottom border ---
     li $a1, 21
     li $a0, 0
 bottom_border_loop:
@@ -95,7 +77,7 @@ bottom_border_loop:
     addi $a0, $a0, 1
     ble $a0, 11, bottom_border_loop
 
-    # Left and right border
+    # --- Left border ---
     li $a0, 0
     li $a1, 0
 left_border_loop:
@@ -103,6 +85,7 @@ left_border_loop:
     addi $a1, $a1, 1
     ble $a1, 21, left_border_loop
 
+    # --- Right border ---
     li $a0, 11
     li $a1, 0
 right_border_loop:
@@ -110,56 +93,8 @@ right_border_loop:
     addi $a1, $a1, 1
     ble $a1, 21, right_border_loop
 
-    jr $ra
-
-# -------------------------------
-# Draw current column (3 gems vertically)
-# -------------------------------
-draw_current_column:
-    lw $t0, current_x
-    lw $t1, current_y
-    la $t2, column_colors
-    li $t3, 0
-
-draw_column_loop:
-    lw $a2, 0($t2)
-    addi $t2, $t2, 4
-
-    move $a0, $t0
-    add $a1, $t1, $t3
-    jal draw_gem
-
-    addi $t3, $t3, 1
-    li $t4, 3
-    blt $t3, $t4, draw_column_loop
-    jr $ra
-
-# -------------------------------
-# Draw gem (8x8 pixels)
-# -------------------------------
-draw_gem:
-    sll $t0, $a0, 3       # x pixel = x unit * 8
-    sll $t1, $a1, 3       # y pixel = y unit * 8
-    li $t2, 0
-
-gem_row_loop:
-    li $t3, 0
-gem_col_loop:
-    add $a0, $t0, $t3
-    add $a1, $t1, $t2
-    jal draw_pixel
-    addi $t3, $t3, 1
-    li $t4, 8
-    blt $t3, $t4, gem_col_loop
-    addi $t2, $t2, 1
-    li $t4, 8
-    blt $t2, $t4, gem_row_loop
-    jr $ra
-
-# -------------------------------
-# Draw static gems (empty for now)
-# -------------------------------
-draw_static_gems:
+    lw $ra, 0($sp)             # restore return address
+    addi $sp, $sp, 4           # restore stack pointer
     jr $ra
 
 # -------------------------------
@@ -178,4 +113,53 @@ draw_pixel:
     add $t3, $t3, $t4
     lw $t4, 0($t3)
     sw $t4, 0($t2)
+    jr $ra
+
+# -------------------------------
+# Draw current column
+# -------------------------------
+draw_current_column:
+    addi $sp, $sp, -8         # save registers
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+
+    lw $t0, current_x         # load x position
+    lw $t1, current_y         # load y position
+
+    # Draw top gem
+    jal generate_random_color  # get random color in $v0
+    move $a2, $v0             # set color argument
+    move $a0, $t0             # set x position
+    move $a1, $t1             # set y position
+    jal draw_pixel            # draw top gem
+
+    # Draw middle gem
+    jal generate_random_color  # get random color in $v0
+    move $a2, $v0             # set color argument
+    move $a0, $t0             # set x position
+    addi $a1, $t1, 1          # set y position + 1
+    jal draw_pixel            # draw middle gem
+
+    # Draw bottom gem
+    jal generate_random_color  # get random color in $v0
+    move $a2, $v0             # set color argument
+    move $a0, $t0             # set x position
+    addi $a1, $t1, 2          # set y position + 2
+    jal draw_pixel            # draw bottom gem
+
+    lw $ra, 0($sp)            # restore registers
+    lw $s0, 4($sp)
+    addi $sp, $sp, 8
+    jr $ra
+
+# -------------------------------
+# Generate random color (1-6)
+# Returns: $v0 - random color index (1-6)
+# -------------------------------
+generate_random_color:
+    li $v0, 42               # random int range syscall
+    li $a0, 0                # random generator id
+    li $a1, 6                # upper bound (exclusive)
+    syscall                  # result in $a0 (0-5)
+    addi $v0, $a0, 1         # convert to 1-6
     jr $ra
